adbb62cd9ffad00da126189bdf4aeec4e0f65837
Matilda-Gabrielsson <maga4117@student.uu.se>
2025-03-09 18:46:23 +0100

    h

--- bfs_split_groups.ts
+++ bfs_split_groups.ts
@@ -14,15 +14,12 @@ import { List } from "../lib/list";
  * @param adj - An array where each index represents a person (node), 
  *              and the value is a List of IDs representing they whish 
  *              to connect with. 
- * @param size - the number of pepole (nodes) in the graph 
+ * @param size - the number of people (nodes) in the graph 
  * @invariant The length of the outer array is size.
  * @invariant Every target node id is a non-negative number less than size.
  * @invariant None of the target node ids appears twice in the same list.
  */
-export type pepole_graph = {
-    adj: Array<List<number>>; 
-    size: number; 
-}; 
+
 
 /**
  * Recursively splits a group into smaller subgroups if it
@@ -35,7 +32,7 @@ export type pepole_graph = {
  *          subgroup of at most `max_group_size` members.
  */
 
-export function split_group_if_to_big (group: number[], max_group_size: number): number[][]{
+export function split_group_if_too_big (group: number[], max_group_size: number): number[][]{
     const result: number[][] = new Array(); 
     if (group.length <= max_group_size) {
         return [group]; 
@@ -44,15 +41,15 @@ export function split_group_if_to_big (group: number[], max_group_size: number):
     const first_half = group.slice(0, mid_first_half_group); 
     const second_half =group.slice(mid_first_half_group); 
 
-    return [...split_group_if_to_big(first_half, max_group_size), 
-            ...split_group_if_to_big(second_half, max_group_size)]; 
+    return [...split_group_if_too_big(first_half, max_group_size), 
+            ...split_group_if_too_big(second_half, max_group_size)]; 
 }
 
 /**
- * Groups the nodes of a given `pepole_graph` into connected components
- * using BFS - Breath First Search. 
- * @param graph - A pepole_graph represneting pepole and there connections. 
- * @returns An array of arrays where each inner array represnets represents a 
+ * Groups the nodes of a given listgraph into connected components
+ * using BFS - Breadth First Search. 
+ * @param graph - A listgraph representing people and their connections. 
+ * @returns An array of arrays where each inner array represents a 
  * BFS-discovered group of connected people.
  */
  
@@ -72,11 +69,11 @@ export function bfs_groups(graph: ListGraph): number[][] {
 }
 
 
-export function merge_bfs_groups(pepole_groups: number[][]): number[][] {
+export function merge_bfs_groups(people_groups: number[][]): number[][] {
     const groups: number[][] = []; 
     const visited = new Set<number>();
 
-    for (const current_group of pepole_groups) {
+    for (const current_group of people_groups) {
         let is_merged = false;
 
         for (let i = 0; i < groups.length; i++) {
@@ -99,18 +96,18 @@ export function merge_bfs_groups(pepole_groups: number[][]): number[][] {
 
 
 /**
- * @param graph - A pepole_graph represneting pepole and there connections. 
+ * @param graph - A people_graph represneting people and there connections. 
  * @param num_groups - The desired number of groups.
  * @returns An array of arrays, where each inner array represents a group.
  *          The length of the outer array is equal to 'num_groups'. 
  */
-export function main_divide_into_groups(graph: pepole_graph, num_groups: number): number[][] {
+export function main_divide_into_groups(graph: ListGraph, num_groups: number): number[][] {
     const BFS_groups = bfs_groups(graph); 
     const max_group_size: number = Math.ceil(graph.size / num_groups); 
     const result: number[][] = []; 
 
     for (let group of BFS_groups) {
-        const split_groups = split_group_if_to_big(group, max_group_size); 
+        const split_groups = split_group_if_too_big(group, max_group_size); 
         result.push(...split_groups);
     }
     return result; 
