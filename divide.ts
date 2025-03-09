462512f77780cadef95325ec395d45cf028de64f
Matilda-Gabrielsson <maga4117@student.uu.se>
2025-03-09 17:14:26 +0100

    tagit bort onödiga kommentarer

--- divide.ts
+++ divide.ts
@@ -11,21 +11,23 @@ export function divide_if_too_many(groups: number[][], number_of_groups: number,
 
     if(groups.length > number_of_groups) {
         const find_small = find_smallest(groups);
-        const smallest_group = find_small.group; //the smallest group
-        const smallest_group_index: number = find_small.index; //the index of the smallest group
+        const smallest_group = find_small.group; 
+        const smallest_group_index: number = find_small.index;
 
-        let groups_new = new Array<Array<number>>(groups.length-1); //gör att det blir en set size på nya arrayen så att den förkortas per körning
+        //create a new array of a set size making it shorter per iteration
+        let groups_new = new Array<Array<number>>(groups.length-1);
         
         
+        //put the groups in a new array shifting them back
         if(smallest_group_index === 0) {
-            for(let i = smallest_group_index; i < groups_new.length; i = i + 1) { //put the groups in a new array shifting them back
+            for(let i = smallest_group_index; i < groups_new.length; i = i + 1) { 
                 groups_new[i] = groups[i + 1];
             }
         } else {
             for(let i = 0; i < smallest_group_index; i = i + 1) {
                 groups_new[i] = groups[i];
             }
-            for(let i = smallest_group_index; i < groups_new.length; i = i + 1) { //put the groups in a new array shifting them back
+            for(let i = smallest_group_index; i < groups_new.length; i = i + 1) { 
                 groups_new[i] = groups[i + 1];
             }
         }
@@ -33,9 +35,7 @@ export function divide_if_too_many(groups: number[][], number_of_groups: number,
         let merged = false;
 
         for(let i = 0; i < groups_new.length; i = i + 1) {
-            if ((groups_new[i].length + smallest_group.length) <= max_group_size) {
-                //Om man kan lägga den minsta gruppen i en grupp utan att den blir för stor görs det
-                //Tänker typ med grupper på en person känns det rimligt
+            if ((groups_new[i].length + smallest_group.length) <= max_group_size) { //if the smallest group can be merged with existing group
                 groups_new[i] = [...smallest_group, ...groups_new[i]];
                 merged = true;
                 break;          
